c9513fd8053eb2ee6c0021f45ea3a00a268bd991
wszgrcy <wszgrcy@gmail.com>
2021-08-13 10:44:35 +0800

    init

---
+++ rollup.config.ts
@@ -0,0 +1,18 @@
+import { RollupOptions } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+import { createTransformer } from "static-injector/transform";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+const config: RollupOptions = {
+  input: "src/index.ts",
+  output: { file: "./dist/main.js", format: "iife" },
+  plugins: [
+    terser({ format: { comments: false } }),
+    nodeResolve(),
+    typescript({
+      tsconfig: "./tsconfig.build.json",
+      transformers: { before: [{ type: "program", factory: (program) => createTransformer(program) }] },
+    }),
+  ],
+};
+export default config;
